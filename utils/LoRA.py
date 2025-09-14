172fa9d94aa558e24cb2850cfeea505c33042973
Sidhant <sidhant96@outlook.com>
2025-09-14 16:15:57 +0530

    reduce eval/save freq

--- utils/LoRA.py
+++ utils/LoRA.py
@@ -152,7 +152,7 @@ def apply_lora(model, tokenizer, batch_size=64, micro_batch_size=4, cutoff_len=2
         args=transformers.TrainingArguments(
             per_device_train_batch_size=micro_batch_size,
             gradient_accumulation_steps=gradient_accumulation_steps,
-            warmup_steps=100,
+            warmup_steps=500,
             num_train_epochs=num_epochs,
             learning_rate=learning_rate,
             fp16=True,
@@ -164,7 +164,7 @@ def apply_lora(model, tokenizer, batch_size=64, micro_batch_size=4, cutoff_len=2
             eval_steps=100,
             save_steps=200,
             output_dir=output_dir,
-            save_total_limit=30,
+            save_total_limit=2,
             load_best_model_at_end=True,
             ddp_find_unused_parameters=None,
             group_by_length=group_by_length,
@@ -321,10 +321,10 @@ def main(args):
             evaluation_strategy="steps",
             save_strategy="steps",
             save_safetensors=False,
-            eval_steps=100,
-            save_steps=200,
+            eval_steps=args.max_steps//4,
+            save_steps=args.max_steps//4,
             output_dir=args.output_dir,
-            save_total_limit=20,
+            save_total_limit=2, # reduce save limit
             load_best_model_at_end=True,
             ddp_find_unused_parameters=None,
             group_by_length=args.group_by_length,
@@ -370,6 +370,7 @@ if __name__ == "__main__":
     parser.add_argument('--val_set_size', type=int, default=2000, help='validation set size')
     parser.add_argument('--prompt_template_name', type=str, default="alpaca", help="The prompt template to use, will default to alpaca.")
     parser.add_argument('--no_instruction', action='store_true', default=False, help="Whether to use the instruction template or not.")
+    parser.add_argument('--max_steps', type=int, default=10000, help='output directory')
 
     # Lora Configuration
     parser.add_argument('--lora_r', type=int, default=8, help='lora r')
